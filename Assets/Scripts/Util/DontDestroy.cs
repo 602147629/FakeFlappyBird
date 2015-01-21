f781bc5724bd3ccaaa312a436d97f8d3cb89acca
liucailin <liu1356@gmail.com>
2015-01-21 22:28:34 +0800

    工具类

---
+++ Assets/Scripts/Util/DontDestroy.cs
@@ -0,0 +1,16 @@
+﻿/// <summary>
+/// Unity GameObject that don't destory on load.
+/// </summary>
+using UnityEngine;
+
+public class DontDestroy : MonoBehaviour {
+
+	public GameObject dontDestroy;
+
+	void Awake()
+	{
+		if (dontDestroy == null) dontDestroy = gameObject;
+		DontDestroyOnLoad(dontDestroy);
+	}
+
+}
