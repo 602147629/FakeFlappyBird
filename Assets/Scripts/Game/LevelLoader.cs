078e47875268ac383eff4fb97b1e71668ec438f5
liucailin <liu1356@gmail.com>
2015-01-31 18:17:49 +0800

    关卡加载类

---
+++ Assets/Scripts/Game/LevelLoader.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+namespace Game
+{
+	public class LevelLoader : MonoBehaviour 
+	{
+		static Action LoadCompleted;
+
+		public static void LoadLevel(string levelName)
+		{
+			Application.LoadLevel(levelName);
+		}
+
+		public static void LoadLevel(string levelName, Action loadCompleted)
+		{
+			Application.LoadLevel(levelName);
+			LoadCompleted = loadCompleted;
+		}
+
+		void OnLevelWasLoaded(int level) 
+		{
+			if (Application.loadedLevel == level)
+			{
+				if (LoadCompleted != null)
+					LoadCompleted();
+			}
+		}
+
+	}
+}
+
