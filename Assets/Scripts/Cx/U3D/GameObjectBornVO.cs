d6484192b060fdc77775cf210c26ce91ea3e97c6
liucailin <liu1356@gmail.com>
2015-02-03 11:15:38 +0800

    born vo，用于创建游戏对象

---
+++ Assets/Scripts/Cx/U3D/GameObjectBornVO.cs
@@ -0,0 +1,59 @@
+using UnityEngine;
+
+namespace CX.U3D
+{
+	/// <summary>
+	/// Use to create a GameObject
+	/// </summary>
+	public class GameObjectBornVO
+	{
+		private string resPath;
+
+		private Vector3 position;
+
+		private Vector3 scale = Vector3.one;
+
+		private Quaternion rotation = Quaternion.identity;
+
+		/// <summary>
+		/// Gets or sets the res path.
+		/// </summary>
+		/// <value>The res path.</value>
+		public string ResPath
+		{
+			get { return resPath; }
+			set { resPath = value; }
+		}
+
+		/// <summary>
+		/// Gets or sets the position.
+		/// </summary>
+		/// <value>The position.</value>
+		public Vector3 Position
+		{
+			get { return position; }
+			set { position = value; }
+		}
+
+		/// <summary>
+		/// Gets or sets the scale.
+		/// </summary>
+		/// <value>The scale.</value>
+		public Vector3 Scale
+		{
+			get { return scale; }
+			set { scale = value; }
+		}
+
+		/// <summary>
+		/// Gets or sets the rotation.
+		/// </summary>
+		/// <value>The rotation.</value>
+		public Quaternion Rotation
+		{
+			get { return rotation; }
+			set { rotation = value; }
+		}
+	}
+}
+
