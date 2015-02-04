177337988587131a2f03fa0973ea1696c588d36c
liucailin <liu1356@gmail.com>
2015-02-04 18:41:16 +0800

    fastJSON dll

---
+++ Assets/Test/Editor/JsonTest.cs
@@ -0,0 +1,31 @@
+﻿using fastJSON;
+using CX.U3D;
+using UnityEngine;
+using NUnit.Framework;
+
+namespace UtilTest
+{
+	[TestFixture]
+	public class JsonTest
+	{
+		[SetUp]
+		public void SetUp()
+		{
+
+		}
+
+		[Test]
+		public void Test()
+		{
+			var vo = new GameObjectBornVO();
+			var jsontext = JSON.ToJSON(vo);
+
+			Debug.Log(jsontext);
+
+			var obj = JSON.ToObject(jsontext);
+			Debug.Log(obj.ToString());
+
+		}
+	}
+}
+
