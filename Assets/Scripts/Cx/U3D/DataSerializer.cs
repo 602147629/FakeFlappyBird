61ae4f5ba47db0017c9378c8210b9f8bd18e5282
liucailin <liu1356@gmail.com>
2015-03-11 01:46:43 +0800

    fix ios serialize???

--- Assets/Scripts/Cx/U3D/DataSerializer.cs
+++ Assets/Scripts/Cx/U3D/DataSerializer.cs
@@ -1,5 +1,6 @@
 using CX.Serialize;
 using UnityEngine;
+using System;
 
 namespace CX.U3D.Serialize
 {
@@ -13,20 +14,27 @@ namespace CX.U3D.Serialize
 		}
 
 		string PersistencePath
-		{
+		{		
 			get { return Application.dataPath + "/../Save/"; }
 		}
 
 		#region ISerializer implementation
 		public void Serialize (object toSerialize)
 		{
+			SetEnviroment();
 			serializer.Serialize(toSerialize);
 		}
 		public T Deserialize<T> ()
 		{
+			SetEnviroment();
 			return serializer.Deserialize<T>();
 		}
 		#endregion
+
+		void SetEnviroment()
+		{
+			Environment.SetEnvironmentVariable("MONO_REFLECTION_SERIALIZER", "yes");
+		}
 	}
 }
 
